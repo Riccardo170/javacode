4db714515305c20ee2d456e4e550b5dc60cefdd2
riccardo <riccagallo67@gmail.com>
2020-11-26 23:43:53 +0100

    javacode

---
+++ URLEncoder.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javadoc_prog;
+import java.lang.Object;
+import java.net.URLEncoder;
+
+/**
+ *
+ * @author riccardo
+ */
+public class URLEncoder {
+    String s;
+    String enc;
+    
+    
+   public void URLEncoder (String s){
+       s=s;
+   }
+   public void URLEncoder (String s, String enc){
+       s=s;
+       enc=enc;
+   }
+   
+   public static String encode(String s){
+       s=s;
+       return s;
+   }
+   public static String encode (String s, String enc){
+       s=s;
+       return s;
+   }
+}
